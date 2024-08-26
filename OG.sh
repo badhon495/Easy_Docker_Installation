87cc7b2f2a253b9df89a2c9056a47fb8f8319b46
badhon <badhon495@gmail.com>
2024-08-26 22:57:20 +0600

    updating OG

--- OG.sh
+++ OG.sh
@@ -21,7 +21,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "bookworm") stable" | \
+  $(. /etc/os-release && echo $VERSION_CODENAME) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 
