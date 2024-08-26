b70a61ecc86424846aa0118e29aa145328ac61a4
badhon <badhon495@gmail.com>
2024-08-26 22:37:20 +0600

    updating the OG

--- OG.sh
+++ OG.sh
@@ -1,5 +1,9 @@
-# to uninstall all the docker previous packages which might create conflicts
+#!/usr/bin/bash
 
+# Redirect all output (stdout) and errors (stderr) to /dev/null
+exec >/dev/null 2>&1
+
+# Uninstall Docker-related packages
 packages="docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc"
 sudo apt remove -y $packages docker-engine docker docker-ce docker-ce-cli docker-compose-plugin
 sudo apt purge -y docker-engine docker docker.io docker-ce docker-compose-plugin
@@ -7,11 +11,8 @@ sudo apt autoremove -y --purge
 sudo rm -rf /var/lib/docker /etc/docker /var/lib/containerd /etc/apparmor.d/docker /var/run/docker.sock
 sudo rm -rf /usr/local/bin/docker-compose*
 
-
-# Now adding the docker repository
-# Here my system is LMDE
-# So, I will be using the debian package manager to install the docker
-# The following commands are for the debian based systems
+# Remove Docker group
+sudo groupdel docker
 
 # Add Docker's official GPG key:
 sudo apt update
@@ -21,20 +22,23 @@ sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyring
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-# If you are using Debian 12 (Bookworm), you can use the following command, otherwise, replace bookworm with your Debian version.
-# To find out your Debian version, run cat /etc/os-release
-
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "bookworm") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 
-
 # Install Docker Engine:
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Add your user to the Docker group:
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 
 # Start and enable the Docker service:
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo systemctl status docker
+
+# Verify the installation:
+docker run hello-world
