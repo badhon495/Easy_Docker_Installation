bf2f4a806cd04a7f0e888536a46ce897f86bbb0c
badhon <badhon495@gmail.com>
2024-08-27 10:26:12 +0600

    Updating Mint

--- Mint.sh
+++ Mint.sh
@@ -14,12 +14,12 @@ sudo groupdel docker
 sudo apt update
 sudo apt install -y ca-certificates curl
 sudo mkdir -p /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add Docker's official repository
 source /etc/os-release
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $UBUNTU_CODENAME stable" | \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $UBUNTU_CODENAME stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 
