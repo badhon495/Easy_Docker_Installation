6849199343a8bf35f32cc8d0e01155819ae719bc
badhon <badhon495@gmail.com>
2024-08-27 12:20:27 +0600

    Adding Debian to automatic file

--- Automatic.sh
+++ Automatic.sh
@@ -6,33 +6,38 @@ if [ -f /etc/os-release ]; then
     . /etc/os-release
     echo "You are using $NAME $VERSION"
 
-    # Check if the distribution is Linux Mint
-    if [ "$NAME" = "Linux Mint" ]; then
-        echo "Linux Mint detected. Executing the command..."
-        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/Mint.sh | bash
-
-    # Check if the distribution is LMDE (Linux Mint Debian Edition)
-    elif [ "$NAME" = "LMDE" ]; then
-        echo "LMDE detected. Executing the command..."
-        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/LMDE.sh | bash
+    #  # Check if the distribution is Fedora
+    if [ "$NAME" = "Fedora" ]; then
+        echo "Fedora detected. Executing the command..."
+        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/Scripts/Fedora.sh | bash
 
     # Check if the distribution is Kali Linux
     elif [ "$NAME" = "Kali GNU/Linux" ]; then
         echo "Kali Linux detected. Executing the command..."
-        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/Kali.sh | bash
+        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/Scripts/Kali.sh | bash
 
-    # Check if the distribution is Fedora
-    elif [ "$NAME" = "Fedora" ]; then
-        echo "Fedora detected. Executing the command..."
-        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/Fedora.sh | bash
+    # Check if the distribution is Debian
+    elif [ "$NAME" = "Debian GNU/Linux" ]; then
+        echo "Debian detected. Executing the command..."
+        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/Scripts/Debian.sh | bash
+    
+    # Check if the distribution is Linux Mint
+    elif [ "$NAME" = "Linux Mint" ]; then
+        echo "Linux Mint detected. Executing the command..."
+        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/Scripts/Mint.sh | bash
+
+    # Check if the distribution is LMDE (Linux Mint Debian Edition)
+    elif [ "$NAME" = "LMDE" ]; then
+        echo "LMDE detected. Executing the command..."
+        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/Scripts/LMDE.sh | bash
 
     # Check if the distribution is Ubuntu
     elif [ "$NAME" = "Ubuntu" ]; then
         echo "Ubuntu detected. Executing the command..."
-        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/Ubuntu.sh | bash
+        curl -sL https://raw.githubusercontent.com/badhon495/Easy_Docker_Installation/main/Scripts/Ubuntu.sh | bash
 
     else
-        echo "Distribution not specifically handled. No command to execute."
+        echo "Distribution not specifically handled. Try the manual installation steps."
     fi
 
 else
